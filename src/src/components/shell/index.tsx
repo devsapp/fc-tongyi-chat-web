45e3a9509bd0fe3d7108073207678fb141626499
青炽 <hanyuntao.hyt@alibaba-inc.com>
2023-05-17 16:20:14 +0800

    chore: hide header

--- src/src/components/shell/index.tsx
+++ src/src/components/shell/index.tsx
@@ -2,22 +2,25 @@ import { AppShell, Header } from '@mantine/core';
 import './index.less';
 import { Nav } from '../navbar';
 import { Chatbox } from '../chatbox';
+import { useGlobalStore } from '../../composerables/state';
 
 export function Shell() {
+  const special = useGlobalStore(state => state.special);
   return (
     <AppShell
       className='app-shell'
       padding="md"
       navbar={<Nav></Nav>}
       header={
-        <>
+        special ? void 0 :
+        (<>
           <Header className="header" height={100} lh="100px" fz="30px">
             <span className='title'>{'快速体验'}</span>
             <span className="subtitle">
               在线体验通义千问
             </span>
           </Header>
-        </>
+        </>)
       }
       styles={(theme) => ({
         main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.white },
