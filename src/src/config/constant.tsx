1884e5e5fcb43bcf66f80c91ca41394977386a7b
青炽 <hanyuntao.hyt@alibaba-inc.com>
2023-05-17 16:10:04 +0800

    style fix

--- src/src/config/constant.tsx
+++ src/src/config/constant.tsx
@@ -9,31 +9,31 @@ export const DEFAULT_CONF = {
         {
             id: 'codegen',
             title: '代码生成',
-            description: '根据用户的输入或需求，自动生成或修改代码，也可以和用户交流，解释或优化代码。通义千问支持多种编程语言，如 Python, Java, C#, JavaScript 等，并且可以在不同的操作系统上运行，如 Mac, Windows, Linux 等。',
+            description: '根据用户的输入或需求，自动生成或修改代码，也可以和用户交流，解释或优化代码。通义千问支持多种编程语言，如 Python, Java, C#, JavaScript 等。',
             builtinPrompts: []
         },
         {
             id: 'literal',
             title: '文案创作',
-            description: '根据用户的输入或需求，自动生成或修改代码，也可以和用户交流，解释或优化代码。通义千问支持多种编程语言，如 Python, Java, C#, JavaScript 等，并且可以在不同的操作系统上运行，如 Mac, Windows, Linux 等。',
+            description: '想要运营没有文案，想要写文章没有思路，把需求告诉通义千问，即可让人工智能帮助你完成文案的创作，帮助你提升效率。',
             builtinPrompts: []
         },
         {
             id: 'tech',
             title: '技术百科',
-            description: '根据用户的输入或需求，自动生成或修改代码，也可以和用户交流，解释或优化代码。通义千问支持多种编程语言，如 Python, Java, C#, JavaScript 等，并且可以在不同的操作系统上运行，如 Mac, Windows, Linux 等。',
+            description: '想要学习新技术，想要查询更多知识，通义千问是你的百科全书，可以作为你的百科助理，为你提供准确靠谱的回答。',
             builtinPrompts: []
         },
         {
             id: 'work',
             title: '工作规划',
-            description: '根据用户的输入或需求，自动生成或修改代码，也可以和用户交流，解释或优化代码。通义千问支持多种编程语言，如 Python, Java, C#, JavaScript 等，并且可以在不同的操作系统上运行，如 Mac, Windows, Linux 等。',
+            description: '如果你想要对自己的工作有什么规划，需要 AI 帮你出谋划策，可以告诉通义千问，通义千问会为你提供参考建议。',
             builtinPrompts: []
         },
         {
             id: 'more',
             title: '更多可能',
-            description: '根据用户的输入或需求，自动生成或修改代码，也可以和用户交流，解释或优化代码。通义千问支持多种编程语言，如 Python, Java, C#, JavaScript 等，并且可以在不同的操作系统上运行，如 Mac, Windows, Linux 等。',
+            description: '通义千问还可以做更多有趣的事情，可以生成图片，可以查看菜谱，可以为你出谋划策。',
             builtinPrompts: []
         },
     ]
