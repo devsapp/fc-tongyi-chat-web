e2d82456ae200f095961f2b0e3bb478c0f3b7cef
青炽 <hanyuntao.hyt@alibaba-inc.com>
2023-05-17 16:11:08 +0800

    style fix

--- src/src/composerables/state.tsx
+++ src/src/composerables/state.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { create } from 'zustand'
-import { DEFAULT_CONF, INITIAL_CONVERSATION } from '../config/constant'
+import { DEFAULT_CONF, INITIAL_CONVERSATION, TONGYI_UID } from '../config/constant'
 import { nanoid } from 'nanoid';
 import { getBuiltinPrompts, getConfig } from '../api';
 import _ from 'lodash'
@@ -33,13 +33,15 @@ export const useGlobalStore = create<any>(devtools((set) => ({
         // if (status) loading({ title:'请稍后', message: '通义千问正在回答中...' })
         return ({ loading: status })
     }),
-    appendConversation: (content: string, from: string) => set(state => ({ history: [ ...state.history, { id: nanoid(), content, status: 'success', from  } ] }))
+    appendLoadingConversation: () => set(state => ({ history: [...state.history, { id: '$loading', content: <span className='ai-loading'>AI正在思考中...</span>, status: 'success', from: TONGYI_UID }] })),
+    appendFailedConversation: (content?: string) => set(state => ({ history: [...state.history, { id: nanoid(), content: content ?? '出错啦，请您换个问题', status: 'failed', from: TONGYI_UID }] })),
+    appendConversation: (content: string, from: string) => set(state => ({ history: [ ...state.history.filter(h => h.id !== '$loading'), { id: nanoid(), content, status: 'success', from  } ] }))
 })))
 
 
 export async function init() {
     const state = useGlobalStore.getState();
-
+    (window as any)._state = state;
     const [ { data: builtinPrompts }, { data: config } ] = await Promise.all([
         getBuiltinPrompts(),
         getConfig()
